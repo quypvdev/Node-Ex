f67ab88c3c8c89397121a087dad204c41ca30d4e
quypham.devplus <quypham.devplus@gmail.com>
2022-03-29 15:49:22 +0700

    feature create member by LIBRARIAN

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const expressValidator = require('express-validator');
+const session = require('express-session');
+require('dotenv').config();
+// import routes
+const authRoutes = require('./src/routes/auth');
+const userRoutes = require('./src/routes/user');
+// const categoryRoutes = require('./routes/category');
+// const productRoutes = require('./routes/product');
+// const braintreeRoutes = require('./routes/braintree');
+// const orderRoutes = require('./routes/order');
+
+
+// app
+const app = express();
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+// db
+mongoose
+.connect(`mongodb+srv://vanquy1306:quypv1306@crudapp.s69oc.mongodb.net/Rentbook?retryWrites=true&w=majority
+
+`,
+    {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+//    useFindAndModify: false,
+   dbName: 'Rentbook'
+
+})
+.then(() => console.log("DB Connected")
+)
+.catch(err=>{
+    console.log(`db error ${err.message}`);
+    process.exit(-1)
+});
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+
+// routes middleware
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+// app.use('/api', categoryRoutes);
+// app.use('/api', productRoutes);
+// app.use('/api', braintreeRoutes);
+// app.use('/api', orderRoutes);
+
+
+// const port = process.env.PORT || 8000;
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+// Production
+var server = app.listen(process.env.PORT || 3000 , function () {
+    var port = server.address().port;
+    console.log(`Server is running on port ${port}`)
+});
