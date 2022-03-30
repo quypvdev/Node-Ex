b7ee9b5ab51066a6d68be8496fec8081106f094b
quypham.devplus <quypham.devplus@gmail.com>
2022-03-30 14:22:29 +0700

    feature authentication book & set default admin

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const {get_books, create_book,update_book,delete_book } = require('../controllers/book');
 
-// const { requireSignin, isAuth, isAdmin} = require('../controllers/auth');
+const { requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 
 // Get all books
 router.get('/listbook', get_books);
@@ -15,12 +15,12 @@ router.get('/listbook', get_books);
 // router.get('/:id/extended', controller.get_book_extended);
 
 // Create book
-router.post('/admin/newbook', create_book);
+router.post('/admin/newbook',requireSignin,isAdmin, create_book);
 
 // Update book
-router.put('/admin/book/:id', update_book);
+router.put('/admin/book/:id',requireSignin,isAdmin, update_book);
 
 // Delete book
-router.delete('/admin/book/delete/:id', delete_book);
+router.delete('/admin/book/delete/:id',requireSignin,isAdmin, delete_book);
 
 module.exports = router;
